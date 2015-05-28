e3ddacaead39e637dd1be9f3ea751a7e86a7a5b0
unknown-user <unknown-user@10.0.0.7>
2015-05-28 17:00:23 -0500

    Slider added

---
+++ NippleDragons/Gruntfile.js
@@ -0,0 +1,24 @@
+﻿module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    uglify: {
+      my_target: {
+        files: {'webDeploy/app.js': ['Scripts/**/*.js']}
+      }
+    },
+    watch: {
+    	scripts: {
+    	files: ['Scripts/**/*.js'],
+    	tasks: ['uglify']
+    	}
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify','watch']);
+
+};
---
+++ NippleDragons/Scripts/app.js
@@ -0,0 +1,33 @@
+﻿triggers.click(function() {
+	if ( !$(this).hasClass('active') ) {
+		target = $(this).index();
+		sliderResponse(target);
+		resetTiming();
+	}
+});
+
+$('.next').click(function() {
+	target = $('ul.triggers li.active').index();
+	target === lastElem ? target = 0 : target = target+1;
+	sliderResponse(target);
+	resetTiming();
+});
+$('.prev').click(function() {
+	target = $('ul.triggers li.active').index();
+	lastElem = triggers.length-1;
+	target === 0 ? target = lastElem : target = target-1;
+	sliderResponse(target);
+	resetTiming();
+});
+
+function sliderTiming() {
+	target = $('ul.triggers li.active').index();
+	target === lastElem ? target = 0 : target = target+1;
+	sliderResponse(target);
+}
+
+var timingRun = setInterval(function() { sliderTiming(); },5000);
+function resetTiming() {
+	clearInterval(timingRun);
+	timingRun = setInterval(function() { sliderTiming(); },5000);
+}
